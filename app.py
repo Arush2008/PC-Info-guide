f7fb9303df68f0012b9ecf96ce2edde98b7c1c5c
HeroicP <arushbasliyal658@gmail.com>
2026-06-22 00:05:12 +1200

    home page almost done just more context and styling needed

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from sqlalchemy import true
 from views import views
 from database import db
 
@@ -13,4 +14,4 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
 
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=true)
