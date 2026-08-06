cb8409d2f60bb28e847986f930f5834d8f53a51d
HeroicP <23779@burnside.school.nz>
2026-08-07 11:21:29 +1200

    added each page for each of the components so user can see each components saperately

--- views.py
+++ views.py
@@ -57,3 +57,57 @@ def learn_component(component):
         return render_template("learn.html", active="")
 
     return render_template("learn.html", active=component)
+
+
+@views.route("/gpu")
+def gpu_list():
+    gpus = GPU.query.all()
+    return render_template("gpu_list.html", gpus=gpus)
+
+
+@views.route("/cpu")
+def cpu_list():
+    cpus = CPU.query.all()
+    return render_template("cpu_list.html", cpus=cpus)
+
+
+@views.route("/motherboard")
+def motherboard_list():
+    motherboards = motherboard.query.all()
+    return render_template("motherboard_list.html", motherboards=motherboards)
+
+
+@views.route("/ram")
+def ram_list():
+    rams = RAM.query.all()
+    return render_template("ram_list.html", rams=rams)
+
+
+@views.route("/storage")
+def storage():
+    storage_items = Storage.query.all()
+    return render_template("storage_list.html", storage=storage_items)
+
+
+@views.route("/psu")
+def psu_list():
+    psus = PSU.query.all()
+    return render_template("psu_list.html", psus=psus)
+
+
+@views.route("/cooler")
+def cooler_list():
+    coolers = Cooler.query.all()
+    return render_template("cooler_list.html", coolers=coolers)
+
+
+@views.route("/case")
+def case_list():
+    cases = Case.query.all()
+    return render_template("case_list.html", cases=cases)
+
+
+@views.route("/case_fans")
+def case_fans_list():
+    case_fans = Fan.query.all()
+    return render_template("case_fans_list.html", case_fans=case_fans)
